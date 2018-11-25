f9a8f649dedc9ca817b22dcae646ca119e884012
dementey <dementeyyy@gmail.com>
2018-11-25 10:29:26 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 if (cluster.isMaster) {
     console.error(`Node cluster master ${process.pid} is running`);
 
-    // Fork workers.
+    // Fork workers
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
